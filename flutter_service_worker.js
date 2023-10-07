393743bb1db91afa990bb0c97c0208acff2b0484
AWahab02 <awzahid02@gmail.com>
2023-10-07 14:44:20 +0500

    m

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ffd41f628f555fe8967b38e92666a814",
-"/": "ffd41f628f555fe8967b38e92666a814",
-"main.dart.js": "b7f5db18909e13639d0a4079f790b980",
+"index.html": "8c120e5d83a7142783289c1a02e193df",
+"/": "8c120e5d83a7142783289c1a02e193df",
+"main.dart.js": "7d18ae81f7821547870126c3c5b40024",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"
 };
